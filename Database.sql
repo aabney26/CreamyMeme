0517269a2d4b42ef168879518bb1009b4593ee28
aabney26 <austin.abney@stu.lee.kyschools.us>
2016-10-27 10:52:01 -0400

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,1 +1,2 @@
-<!--I think I will instead use a sql client and somehow correlate that within this hub.-->
+<!--I think I will instead use a sql client and somehow correlate that within this hub.
+MySQL, probably-->
