de549931ce23c5196f0521d85007502f6ecb0634
aabney26 <austin.abney@stu.lee.kyschools.us>
2016-12-05 10:33:43 -0500

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,2 +1,9 @@
 <!--I think I will instead use a sql client and somehow correlate that within this hub. MySQL, probably-->
-<!--Talk to Ms. Neace about getting MySQL on this computer.-->
+
+create table employee
+(first varchar(15),
+ last varchar(20),
+ age number(3),
+ address varchar(30),
+ city varchar(20),
+ state varchar(20));
