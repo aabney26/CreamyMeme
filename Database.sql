037c0027325f5ee6b220e638ca43f16443aa1238
aabney26 <austin.abney@stu.lee.kyschools.us>
2017-01-03 10:32:10 -0500

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,10 +1,4 @@
-/*I think I will instead use a sql client and somehow correlate that within this hub. MySQL, probably*/
-/*Look at this webpage!!! - http://www.w3schools.com/SQl/default.asp */
+--I think I will instead use a sql client and somehow correlate that within this hub. MySQL, probably.
+--Look at this webpage!!! - http://www.w3schools.com/SQl/default.asp
 
-create table picture
-(first varchar(15),
- last varchar(20),
- age number(3),
- address varchar(30),
- city varchar(20),
- state varchar(20));
+create database CreamyMeme;
